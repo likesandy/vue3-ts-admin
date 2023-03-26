0b1249cf4337949b724ff8fc904d0f6254ac7b36
ilun <1445709137@qq.com>
2023-03-26 22:01:44 +0800

    ✨ feat(配置MockPlugin):

--- vite/plugins/index.ts
+++ vite/plugins/index.ts
@@ -1,7 +1,12 @@
 import vue from '@vitejs/plugin-vue'
 import type { PluginOption } from 'vite'
-const setupPlugins = (isBuild: boolean, env: Record<string, string>): PluginOption[] => {
-  return [vue()]
+import setupMockPlugin from './mock'
+const setupPlugins = (isBuild: boolean, env: Record<string, string>) => {
+  const plugins: PluginOption[] = []
+  plugins.push(vue())
+  plugins.push(setupMockPlugin(isBuild))
+
+  return plugins
 }
 
 export default setupPlugins
